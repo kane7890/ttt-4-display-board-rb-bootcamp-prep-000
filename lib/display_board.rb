d96283f821c1a9e202b602d61fafd54d6787d9f8
Kane Koo <kanekoo@gmail.com>
2018-11-30 21:08:04 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,12 +2,15 @@
 # out the current state.
 def display_board (board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
+#<<<<<<< HEAD
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-  puts "-----------"
+ # puts "-----------"
+# =======
+#  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+ # puts " #{board[6]} | #{board[7]} | #{board[8]} "
+# >>>>>>> 4b4aef9f912fae575b04b8088f23e7536bf7c0db
   
 end
-board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
-display_board(board)
